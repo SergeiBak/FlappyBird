5ca8b84e69713d2b6c3249814d8cc1cbe1e06b57
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2021-12-18 21:28:46 -0800

    Menu Interactions
    Linked UI to gameController and set up Play, Pause, and Gameover states

--- projectCode/Flappy Bird/Assets/Scripts/GameController.cs
+++ projectCode/Flappy Bird/Assets/Scripts/GameController.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField]
+    private Player player;
+
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private GameObject playButton;
+    [SerializeField]
+    private GameObject gameOver;
+
     private int score;
+
+    private void Awake()
+    {
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        player.enabled = false;
+    }
 
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
 
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
 }

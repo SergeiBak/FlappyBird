8fe1c3e74b787a9cf5739bbc47ae11b16bd36ab5
SergeiBak <77221025+SergeiBak@users.noreply.github.com>
2021-12-18 22:41:48 -0800

    Player rotation
    Added rotation for player based on whether or not they are falling. Also adjusted variable values to make the game more difficult.

--- projectCode/Flappy Bird/Assets/Scripts/Player.cs
+++ projectCode/Flappy Bird/Assets/Scripts/Player.cs
@@ -17,11 +17,21 @@ public class Player : MonoBehaviour
 
     private GameController gm;
 
+    private Vector2 lastPos;
+    [SerializeField]
+    private float rotateSpeed =  90f;
+
+    private float nextCheck;
+    [SerializeField]
+    private float checkDelay = 0.1f;
+
     // Start is called before the first frame update
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
         gm = FindObjectOfType<GameController>();
+
+        nextCheck = Time.time;
     }
 
     private void Start()
@@ -58,6 +68,38 @@ public class Player : MonoBehaviour
 
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+
+        if (Time.time < nextCheck)
+        {
+            return;
+        }
+
+        if (IsFalling())
+        {
+            transform.Rotate(transform.forward, -rotateSpeed * Time.smoothDeltaTime);
+            if (transform.eulerAngles.z < -30)
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, -30);
+            }
+        }
+        else
+        {
+            transform.Rotate(transform.forward, (rotateSpeed * 2) * Time.smoothDeltaTime);
+
+            if (transform.eulerAngles.z > 30)
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 30);
+            }
+        }
+
+        lastPos = transform.position;
+
+        nextCheck = Time.time + checkDelay;
+    }
+
+    bool IsFalling()
+    {
+        return (transform.position.y < lastPos.y);
     }
 
     private void AnimateBird()
